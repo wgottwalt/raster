b2d0570cce50b09a820aa830111d3d3c07884ce9
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2020-08-23 16:35:39 +0200

    added color component RGBA16161616
    This is the base pixel data class used by all other componentes if possible. It
    provides 16bit color channel precision.

---
+++ sources/Color/RGBA16161616.cxx
@@ -0,0 +1,140 @@
+#include <limits>
+#include <utility>
+#include "RGBA16161616.hxx"
+
+namespace Color
+{
+    //--- internal stuff ---
+
+    static const uint16_t Max = std::numeric_limits<uint16_t>::max();
+
+    //--- public constructors ---
+
+    RGBA16161616::RGBA16161616() noexcept
+#if defined __ORDER_LITTLE_ENDIAN__
+    : a(Max), b(0), g(0), r(0)
+#elif defined __ORDER_BIG_ENDIAN__
+    : r(0), g(0), b(0), a(Max)
+#else
+#error "fucked up endianness"
+#endif
+    {
+    }
+
+    RGBA16161616::RGBA16161616(const uint64_t val) noexcept
+    : value(val)
+    {
+    }
+
+    RGBA16161616::RGBA16161616(const uint16_t rr, const uint16_t gg, const uint16_t bb,
+                               const uint16_t aa) noexcept
+#if defined __ORDER_LITTLE_ENDIAN__
+    : a(aa), b(bb), g(gg), r(rr)
+#elif defined __ORDER_BIG_ENDIAN__
+    : r(rr), g(gg), b(bb), a(aa)
+#else
+#error "fucked up endianness"
+#endif
+    {
+    }
+
+    RGBA16161616::RGBA16161616(const uint8_t rrh, const uint8_t rrl, const uint8_t ggh,
+                               const uint8_t ggl, const uint8_t bbh, const uint8_t bbl,
+                               const uint8_t aah, const uint8_t aal) noexcept
+#if defined __ORDER_LITTLE_ENDIAN__
+    : al(aal), ah(aah), bl(bbl), bh(bbh), gl(ggl), gh(ggh), rl(rrl), rh(rrh)
+#elif defined __ORDER_BIG_ENDIAN__
+    : rh(rrh), rl(rrl), gh(ggh), gl(ggl), bh(bbh), bl(bbl), ah(aah), al(aal)
+#else
+#error "fucked up endianness"
+#endif
+    {
+    }
+
+    RGBA16161616::RGBA16161616(const RGBA16161616 &rhs) noexcept
+    : value(rhs.value)
+    {
+    }
+
+    RGBA16161616::RGBA16161616(RGBA16161616 &&rhs) noexcept
+    : value(std::move(rhs.value))
+    {
+    }
+
+    RGBA16161616::~RGBA16161616() noexcept
+    {
+    }
+
+    //--- public operators ---
+
+    RGBA16161616 &RGBA16161616::operator=(const RGBA16161616 &rhs) noexcept
+    {
+        value = rhs.value; // moving a 64 bit value is less expansive than a branch
+
+        return *this;
+    }
+
+    RGBA16161616 &RGBA16161616::operator=(RGBA16161616 &&rhs) noexcept
+    {
+        value = std::move(rhs.value);
+
+        return *this;
+    }
+
+    bool RGBA16161616::operator==(const RGBA16161616 &rhs) const noexcept
+    {
+        return value == rhs.value;
+    }
+
+    bool RGBA16161616::operator!=(const RGBA16161616 &rhs) const noexcept
+    {
+        return value != rhs.value;
+    }
+
+    //--- public methods ---
+
+    RGBA16161616 &RGBA16161616::set(const uint64_t val) noexcept
+    {
+        value = val;
+
+        return *this;
+    }
+
+    RGBA16161616 &RGBA16161616::set(const uint16_t rr, const uint16_t gg, const uint16_t bb,
+                                    const uint16_t aa) noexcept
+    {
+        r = rr;
+        g = gg;
+        b = bb;
+        a = aa;
+
+        return *this;
+    }
+
+    RGBA16161616 &RGBA16161616::set(const uint8_t rrh, const uint8_t rrl, const uint8_t ggh,
+                                    const uint8_t ggl, const uint8_t bbh, const uint8_t bbl,
+                                    const uint8_t aah, const uint8_t aal) noexcept
+    {
+        rh = rrh;
+        rl = rrl;
+        gh = ggh;
+        gl = ggl;
+        bh = bbh;
+        bl = bbl;
+        ah = aah;
+        al = aal;
+
+        return *this;
+    }
+
+    uint16_t RGBA16161616::averageRGB() const noexcept
+    {
+        return (static_cast<uint32_t>(r) + static_cast<uint32_t>(g) + static_cast<uint32_t>(b)) / 3;
+    }
+
+    uint16_t RGBA16161616::averageRGBA() const noexcept
+    {
+        return (static_cast<uint32_t>(r) + static_cast<uint32_t>(g) + static_cast<uint32_t>(b) +
+                static_cast<uint32_t>(a)) / 4;
+    }
+}
---
+++ sources/Color/RGBA16161616.hxx
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Color
+{
+    class RGBA16161616 {
+    public:
+        //--- public types and constants ---
+        static constexpr uint64_t Black = 0x000000000000FFFFull;
+        static constexpr uint64_t White = 0xFFFFFFFFFFFFFFFFull;
+        static constexpr uint64_t Red = 0xFFFF00000000FFFFull;
+        static constexpr uint64_t Green = 0x0000FFFF0000FFFFull;
+        static constexpr uint64_t Blue = 0x00000000FFFFFFFFull;
+
+        //--- public properties ---
+        union {
+            uint64_t value;
+#if defined __ORDER_LITTLE_ENDIAN__
+            struct {
+                uint16_t a, b, g, r;
+            };
+            struct {
+                uint8_t al, ah, bl, bh, gl, gh, rl, rh;
+            };
+            struct {
+                char c8, c7, c6, c5, c4, c3, c2, c1;
+            };
+#elif defined __ORDER_BIG_ENDIAN__
+            struct {
+                uint16_t r, g, b, a;
+            };
+            struct {
+                uint8_t rh, rl, gh, gl, bh, bl, ah, al;
+            };
+            struct {
+                char c1, c2, c3, c4, c5, c6, c7, c8;
+            };
+#else
+#error "fucked up endianness"
+#endif
+        };
+
+        //--- public constructors ---
+        RGBA16161616() noexcept;
+        RGBA16161616(const uint64_t val) noexcept;
+        RGBA16161616(const uint16_t rr, const uint16_t gg, const uint16_t bb,
+                     const uint16_t aa) noexcept;
+        RGBA16161616(const uint8_t rrh, const uint8_t rrl, const uint8_t ggh, const uint8_t ggl,
+                     const uint8_t bbh, const uint8_t bbl, const uint8_t aah,
+                     const uint8_t aal) noexcept;
+        RGBA16161616(const RGBA16161616 &rhs) noexcept;
+        RGBA16161616(RGBA16161616 &&rhs) noexcept;
+        ~RGBA16161616() noexcept;
+
+        //--- public operators ---
+        RGBA16161616 &operator=(const RGBA16161616 &rhs) noexcept;
+        RGBA16161616 &operator=(RGBA16161616 &&rhs) noexcept;
+        bool operator==(const RGBA16161616 &rhs) const noexcept;
+        bool operator!=(const RGBA16161616 &rhs) const noexcept;
+
+        //--- public methods ---
+        RGBA16161616 &set(const uint64_t val) noexcept;
+        RGBA16161616 &set(const uint16_t rr, const uint16_t gg, const uint16_t bb,
+                          const uint16_t aa) noexcept;
+        RGBA16161616 &set(const uint8_t rrh, const uint8_t rrl, const uint8_t ggh,
+                          const uint8_t ggl, const uint8_t bbh, const uint8_t bbl,
+                          const uint8_t aah, const uint8_t aal) noexcept;
+        uint16_t averageRGB() const noexcept;
+        uint16_t averageRGBA() const noexcept;
+    };
+}
