8284935190775eb5cd57bfb8611277bed0a1291a
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2020-09-13 16:16:44 +0200

    added experimental sphere 3d object class

---
+++ sources/Math/Sphere.hxx
@@ -0,0 +1,115 @@
+#pragma once
+
+#include "Shape.hxx"
+
+namespace Math
+{
+    template <Common::Concept::Number T>
+    class Sphere : public Shape<T> {
+    public:
+        //--- public types and constants ---
+        static constexpr T MinR = 0;
+        static constexpr T MaxR = std::numeric_limits<T>::max();
+
+        //--- public constructors ---
+        Sphere() noexcept
+        : Shape<T>(), _radius()
+        {
+        }
+
+        Sphere(const Vector3<T> &org, const T rad) noexcept
+        : Shape<T>(org), _radius(rad)
+        {
+        }
+
+        Sphere(const Sphere &rhs) noexcept
+        : Shape<T>(rhs), _radius(rhs._radius)
+        {
+        }
+
+        Sphere(Sphere &&rhs) noexcept
+        : Shape<T>(std::move(rhs)), _radius(std::move(rhs._radius))
+        {
+        }
+
+        virtual ~Sphere() noexcept
+        {
+        }
+
+        //--- public operators ---
+        Sphere &operator=(const Sphere &rhs) noexcept
+        {
+            if (this != &rhs)
+            {
+                Shape<T>::operator=(rhs);
+                _radius = rhs._radius;
+            }
+
+            return *this;
+        }
+
+        Sphere &operator=(Sphere &&rhs) noexcept
+        {
+            if (this != &rhs)
+            {
+                Shape<T>::operator=(std::move(rhs));
+                _radius = std::move(rhs._radius);
+            }
+
+            return *this;
+        }
+
+        bool operator==(const Sphere &rhs) const noexcept
+        {
+            return Shape<T>::operator==(rhs) &&
+                   _radius == rhs._radius;
+        }
+
+        bool operator!=(const Sphere &rhs) const noexcept
+        {
+            return !(*this == rhs);
+        }
+
+        //--- public methods ---
+        T radius() const noexcept
+        {
+            return _radius;
+        }
+
+        bool setRadius(const T val) noexcept
+        {
+            if (Common::Tools::inRange(val, MinR, MaxR))
+            {
+                _radius = val;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        virtual Sphere::Intersection intersect(const Ray<T> &ray) const override final
+        {
+            const Vector3<T> diff = Shape<T>::origin() - ray.origin();
+
+            if (const T a = diff.dotProduct(ray.direction()); a > 0)
+            {
+                const T r2 = _radius * _radius;
+
+                if (const T b = diff.dotProduct(diff) - (a * a); b <= r2)
+                {
+                    const T c = std::sqrt(r2 - b);
+
+                    return {true, std::max(Shape<T>::MinRange, a - c),
+                            std::min(a + c, Shape<T>::MaxRange)};
+                }
+            }
+
+            return {false, 0, 0};
+        }
+
+    private:
+        //--- private properties ---
+        T _radius;
+    };
+}
