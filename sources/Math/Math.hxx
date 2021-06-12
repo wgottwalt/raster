e98182b7998bdefc9a0e241990c1f17354a380dd
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-06-12 10:08:35 +0200

    removed sources belong to raytracing, will be redone later

--- sources/Math/Math.hxx
+++ sources/Math/Math.hxx
@@ -1,5 +1,3 @@
 #pragma once
 
 #include "Vector.hxx"
-#include "Ray.hxx"
-#include "Matrix.hxx"
--- sources/Object/Object.hxx
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Shape.hxx"
-#include "Camera.hxx"
-#include "Plane.hxx"
-#include "Sphere.hxx"
