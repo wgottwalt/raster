30df636cef364b41166c8399b880eda2de3a6ab5
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-07-17 08:34:41 +0200

    added missing Common namespace encapsulation to Args class

--- sources/Common/Args.cxx
+++ sources/Common/Args.cxx
@@ -1,112 +1,115 @@
 #include <cctype>
 #include "Args.hxx"
 
-//--- public constructors ---
-
-Args::Args(int32_t &argc, char **argv, const HelpFunc func) noexcept(false)
-: _argc{argc}, _argv{argv}, _func{func}, _help_called{false}
+namespace Common
 {
-    _help_called = parseHelp();
-}
+    //--- public constructors ---
 
-Args::~Args() noexcept
-{
-}
-
-//--- public methods ---
-
-bool Args::help(const bool force) const noexcept(false)
-{
-    if ((_help_called || force) && _func)
+    Args::Args(int32_t &argc, char **argv, const HelpFunc func) noexcept(false)
+    : _argc{argc}, _argv{argv}, _func{func}, _help_called{false}
     {
-        _func(_argv[0]);
-        return true;
+        _help_called = parseHelp();
     }
 
-    return false;
-}
-
-Args::Result Args::longOpt(const std::string &param, const bool has_value) const noexcept(false)
-{
-    if (!param.empty())
+    Args::~Args() noexcept
     {
-        const size_t psize{param.size()};
+    }
 
-        for (int32_t i{1}; i < _argc; ++i)
+    //--- public methods ---
+
+    bool Args::help(const bool force) const noexcept(false)
+    {
+        if ((_help_called || force) && _func)
         {
-            const std::string arg{_argv[i]};
-            const size_t asize{arg.size()};
+            _func(_argv[0]);
+            return true;
+        }
 
-            if ((asize <= 2) || (arg.substr(0, 2) != "--"))
-                continue;
+        return false;
+    }
 
-            if (arg.substr(2, psize) == param)
+    Args::Result Args::longOpt(const std::string &param, const bool has_value) const noexcept(false)
+    {
+        if (!param.empty())
+        {
+            const size_t psize{param.size()};
+
+            for (int32_t i{1}; i < _argc; ++i)
             {
-                if (has_value)
-                {
-                    if ((asize > (psize + 3)) && (arg[psize + 2] == '='))
-                        return {true, arg.substr(psize + 3, std::string::npos)};
+                const std::string arg{_argv[i]};
+                const size_t asize{arg.size()};
 
-                    if ((asize == (psize + 2)) && ((i + 1) < _argc) && (_argv[i + 1][0] != '-'))
-                        return {true, _argv[i + 1]};
-                }
-                else
+                if ((asize <= 2) || (arg.substr(0, 2) != "--"))
+                    continue;
+
+                if (arg.substr(2, psize) == param)
                 {
-                    if (asize == (psize + 2))
-                        return {true, ""};
+                    if (has_value)
+                    {
+                        if ((asize > (psize + 3)) && (arg[psize + 2] == '='))
+                            return {true, arg.substr(psize + 3, std::string::npos)};
+
+                        if ((asize == (psize + 2)) && ((i + 1) < _argc) && (_argv[i + 1][0] != '-'))
+                            return {true, _argv[i + 1]};
+                    }
+                    else
+                    {
+                        if (asize == (psize + 2))
+                            return {true, ""};
+                    }
                 }
             }
         }
+
+        return {false, ""};
     }
 
-    return {false, ""};
-}
+    Args::Result Args::shortOpt(const char param, const bool has_value) const noexcept(false)
+    {
+        if (std::isalnum(param))
+        {
+            for (int32_t i = 1; i < _argc; ++i)
+            {
+                const std::string arg{_argv[i]};
+                const size_t asize{arg.size()};
 
-Args::Result Args::shortOpt(const char param, const bool has_value) const noexcept(false)
-{
-    if (std::isalnum(param))
+                if ((asize <= 1) || (arg[0] != '-'))
+                    continue;
+
+                if ((asize > 1) && (arg[1] == param))
+                {
+                    if (has_value)
+                    {
+                        if ((asize > 3) && (arg[2] == '='))
+                            return {true, arg.substr(3, std::string::npos)};
+
+                        if ((asize == 2) && ((i + 1) < _argc) && (_argv[i + 1][0] != '-'))
+                            return {true, _argv[i + 1]};
+                    }
+                    else
+                    {
+                        if (asize == 2)
+                            return {true, ""};
+                    }
+                }
+            }
+        }
+
+        return {false, ""};
+    }
+
+    //--- protected methods ---
+
+    bool Args::parseHelp() const noexcept(false)
     {
         for (int32_t i = 1; i < _argc; ++i)
         {
-            const std::string arg{_argv[i]};
-            const size_t asize{arg.size()};
+            const std::string_view arg{_argv[i]};
 
-            if ((asize <= 1) || (arg[0] != '-'))
-                continue;
-
-            if ((asize > 1) && (arg[1] == param))
-            {
-                if (has_value)
-                {
-                    if ((asize > 3) && (arg[2] == '='))
-                        return {true, arg.substr(3, std::string::npos)};
-
-                    if ((asize == 2) && ((i + 1) < _argc) && (_argv[i + 1][0] != '-'))
-                        return {true, _argv[i + 1]};
-                }
-                else
-                {
-                    if (asize == 2)
-                        return {true, ""};
-                }
-            }
+            if ((arg == "--help") || (arg == "-h"))
+                return true;
         }
+
+        return false;
     }
-
-    return {false, ""};
-}
-
-//--- protected methods ---
-
-bool Args::parseHelp() const noexcept(false)
-{
-    for (int32_t i = 1; i < _argc; ++i)
-    {
-        const std::string_view arg{_argv[i]};
-
-        if ((arg == "--help") || (arg == "-h"))
-            return true;
-    }
-
-    return false;
 }
--- sources/Common/Args.hxx
+++ sources/Common/Args.hxx
@@ -4,35 +4,38 @@
 #include <string>
 #include <tuple>
 
-class Args {
-public:
-    //--- public types and constants ---
-    using HelpFunc = std::function<void (const std::string &)>;
-    using Result = std::tuple<bool,const std::string>;
+namespace Common
+{
+    class Args {
+    public:
+        //--- public types and constants ---
+        using HelpFunc = std::function<void (const std::string &)>;
+        using Result = std::tuple<bool,const std::string>;
 
-    //--- public constructors ---
-    Args(int32_t &argc, char **argv, const HelpFunc func) noexcept(false);
-    Args(const Args &rhs) noexcept(false) = delete;
-    Args(Args &&rhs) noexcept = delete;
-    ~Args() noexcept;
+        //--- public constructors ---
+        Args(int32_t &argc, char **argv, const HelpFunc func) noexcept(false);
+        Args(const Args &rhs) noexcept(false) = delete;
+        Args(Args &&rhs) noexcept = delete;
+        ~Args() noexcept;
 
-    //--- public operators ---
-    Args &operator=(const Args &rhs) noexcept(false) = delete;
-    Args &operator=(Args &&rhs) noexcept = delete;
+        //--- public operators ---
+        Args &operator=(const Args &rhs) noexcept(false) = delete;
+        Args &operator=(Args &&rhs) noexcept = delete;
 
-    //--- public methods ---
-    bool help(const bool force = false) const noexcept(false);
-    Result longOpt(const std::string &param, const bool has_value = false) const noexcept(false);
-    Result shortOpt(const char param, const bool has_value = false) const noexcept(false);
+        //--- public methods ---
+        bool help(const bool force = false) const noexcept(false);
+        Result longOpt(const std::string &param, const bool has_value = false) const noexcept(false);
+        Result shortOpt(const char param, const bool has_value = false) const noexcept(false);
 
-protected:
-    //--- protected methods ---
-    bool parseHelp() const noexcept(false);
+    protected:
+        //--- protected methods ---
+        bool parseHelp() const noexcept(false);
 
-private:
-    //--- private properties ---
-    int32_t &_argc;
-    char **_argv;
-    HelpFunc _func;
-    bool _help_called;
-};
+    private:
+        //--- private properties ---
+        int32_t &_argc;
+        char **_argv;
+        HelpFunc _func;
+        bool _help_called;
+    };
+}
